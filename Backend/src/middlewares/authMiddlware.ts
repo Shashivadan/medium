14c9d4967d42f6ffbff27ac2a82a6dae3f3894a7
Shashivadan <shashivadan99@gmail.com>
2024-05-03 00:45:23 +0530

    changes: auth middleware

--- Backend/src/middlewares/authMiddlware.ts
+++ Backend/src/middlewares/authMiddlware.ts
@@ -4,14 +4,10 @@ import { verify } from "hono/jwt";
 
 export async function authMiddlware(c: Context, next: Next) {
   try {
-    const token = c.req.header("token")
-    if (!token) {
-      return c.json({ message: "Token Is Not Provided" }, 403)
-    }
+    const token: string | undefined = c.req.header("token")
+    if (!token) return c.json({ message: "Token Is Not Provided" }, 403)
     const decode = await verify(token, c.env.JWT_SECRET)
-    if (!decode) {
-      return c.json({ error: "unauthized" }, 401)
-    }
+    if (!decode) return c.json({ error: "unauthized" }, 401)
     c.set("userId", decode.id)
     await next()
   } catch (error) {
