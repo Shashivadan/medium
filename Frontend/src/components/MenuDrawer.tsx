c745fa7811cb201fb80deb4f9cfed9a4564e1c6a
Shashivadan <shashivadan99@gmail.com>
2024-05-24 20:46:30 +0530

    some changes

--- Frontend/src/components/MenuDrawer.tsx
+++ Frontend/src/components/MenuDrawer.tsx
@@ -5,17 +5,25 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
 
 function MenuDrawer() {
+  const navigate = useNavigate();
   return (
     <div>
       <Drawer>
         <DrawerTrigger>menu</DrawerTrigger>
 
         <DrawerContent className=" flex flex-col gap-3 px-3">
-          <Button variant={"outline"}>write</Button>
-          <Button variant={"outline"}>blogs</Button>
-          <Button variant={"outline"}>logout</Button>
+          <Button onClick={() => navigate("/write")} variant={"outline"}>
+            write
+          </Button>
+          <Button onClick={() => navigate("/blogs")} variant={"outline"}>
+            blogs
+          </Button>
+          <Button onClick={() => navigate("/signin")} variant={"outline"}>
+            logout
+          </Button>
           <DrawerClose>
             <Button variant={"outline"}>cancel</Button>
           </DrawerClose>
