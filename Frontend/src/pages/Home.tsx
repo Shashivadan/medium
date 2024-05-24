e3ef7612bdeb16be3339c22229dcdfcd37aa9b1f
Shashivadan <shashivadan99@gmail.com>
2024-05-24 20:50:41 +0530

    some changes

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     <>
       <div className=" h-screen bg-black text-white ">
         <div className="h-full w-full flex  items-center justify-center text-center  ">
-          <div className="w-[30%] text-center flex flex-col items-center gap-5">
-            <div className="text-6xl font-bold">
+          <div className=" p-10 md:p-0 md:w-[30%] text-center flex flex-col items-center gap-5">
+            <div className=" text-4xl md:text-6xl font-bold">
               Good things come those{" "}
               <em className=" font-thin  font-serif">who wait.</em>
             </div>
-            <div className="  w-[70%] font-semibold text-slate-300 ">
+            <div className="text-sm  md:w-[70%] font-semibold text-slate-300 ">
               Launch your very own waitlist, grow your mailing list all while
               building the hype for launch day.
             </div>
