122f428d89e32e53365cdbf075dcb6aa58131609
Shashivadan <shashivadan99@gmail.com>
2024-05-25 12:52:48 +0530

    added animation on home page4

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,28 +1,58 @@
 import { Link } from "react-router-dom";
-
+import { motion } from "framer-motion";
 export default function Home() {
+  const parentVarient = {
+    initial: {
+      opacity: 0,
+      y: -100,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+  const childVarient = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
   return (
     <>
       <div className=" h-screen bg-black text-white ">
         <div className="h-full w-full flex  items-center justify-center text-center  ">
-          <div className=" p-10 md:p-0 md:w-[30%] text-center flex flex-col items-center gap-5">
-            <div className=" text-4xl md:text-6xl font-bold">
+          <motion.div
+            variants={parentVarient}
+            initial="initial"
+            animate="animate"
+            transition={{
+              duration: 1,
+            }}
+            className=" p-10 md:p-0 md:w-[30%] text-center flex flex-col items-center gap-5"
+          >
+            <motion.div
+              variants={childVarient}
+              className=" text-4xl md:text-6xl font-bold"
+            >
               Good things come those{" "}
               <em className=" font-thin  font-serif">who wait.</em>
-            </div>
-            <div className="text-sm  md:w-[70%] font-semibold text-slate-300 ">
+            </motion.div>
+            <motion.div
+              variants={childVarient}
+              className="text-sm  md:w-[70%] font-semibold text-slate-300 "
+            >
               Launch your very own waitlist, grow your mailing list all while
               building the hype for launch day.
-            </div>
+            </motion.div>
             <div>
-              <Link
-                to={"/signin"}
-                className=" font-semibold px-9 py-2 bg-transparent   border-2 rounded-lg "
-              >
-                Get Started
-              </Link>
+              <motion.div variants={childVarient}>
+                <Link
+                  to={"/signin"}
+                  className=" font-semibold px-9 py-2 bg-transparent   border-2 rounded-lg "
+                >
+                  Get Started
+                </Link>
+              </motion.div>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
